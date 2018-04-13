6d51a0278a590a95ede60919403f14b29aa46424
cwang101 <625250454@qq.com>
2018-04-13 16:12:15 +0800

    The practice10 complete!

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -1,4 +1,47 @@
 package practice10;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+    int number;
+    Student leader;
+    List<Student> members=new ArrayList<>();
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName(){
+        StringBuilder sb=new StringBuilder();
+        sb.append("Class ").append(number);
+        return sb.toString();
+    }
+    public void assignLeader(Student leader){
+        if(members.contains(leader)){
+            this.leader=leader;
+        }else {
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+    public void appendMember(Student member){
+        members.add(member);
+    }
+
+    public boolean isIn(Student student){
+
+        for (Student member:members){
+            if(member.getId()==student.getId()){
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,66 @@
 package practice10;
 
-public class Teacher {
+import java.util.HashSet;
+import java.util.Set;
+
+public class Teacher extends Person{
+
+    Set<Klass> classes=new HashSet<>();
+    public Teacher(int id,String name, int age, Set<Klass> classes) {
+        super(id,name, age);
+        this.classes = classes;
+    }
+
+    public Teacher(int id,String name, int age) {
+        super(id,name, age);
+    }
+
+    public Set<Klass> getClasses() {
+        return classes;
+    }
+
+    public String introduce(){
+        StringBuilder sb=new StringBuilder();
+        sb.append(super.introduce()).append(" I am a Teacher. I teach ");
+        if(classes.size()>0){
+            sb.append(getClassDisplay()).append(".");
+        }else{
+            sb.append("No Class.");
+        }
+
+        return sb.toString();
+    }
+
+    public boolean isTeaching(Student student){
+        for(Klass klass:classes){
+            if(klass.isIn(student)){
+                return true;
+            }
+        }
+        return false;
+    }
+    public String introduceWith(Student student){
+        StringBuilder sb=new StringBuilder();
+        if(isTeaching(student)){
+            sb.append(super.introduce()).append(" I am a Teacher. I teach ").append(student.getName()).append(".");
+        }else {
+            sb.append(super.introduce()).append(" I am a Teacher. I don't teach ").append(student.getName()).append(".");
+
+        }
+        return sb.toString();
+    }
+
+    private String getClassDisplay(){
+        StringBuilder sb=new StringBuilder();
+        sb.append("Class ");
+        int index=0;
+        for(Klass klass:classes){
+            sb.append(klass.getNumber());
+            if(index<classes.size()-1){
+                sb.append(", ");
+            }
+            index++;
+        }
+        return sb.toString();
+    }
 }
