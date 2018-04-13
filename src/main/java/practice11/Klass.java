5b3085ccb2c47520c9adb1961946fc12d2e5eec8
cwang101 <625250454@qq.com>
2018-04-13 16:36:21 +0800

    The practice11 complete!

--- src/main/java/practice11/Klass.java
+++ src/main/java/practice11/Klass.java
@@ -1,4 +1,64 @@
 package practice11;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Klass {
+    int number;
+    Student leader;
+    Teacher teacher;
+    Set<Student> members=new HashSet<>();
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName(){
+        StringBuilder sb=new StringBuilder();
+        sb.append("Class ").append(number);
+        return sb.toString();
+    }
+    public void assignLeader(Student leader){
+        if(members.contains(leader)){
+            this.leader=leader;
+            StringBuilder sb=new StringBuilder();
+            if(teacher!=null) {
+                sb.append(teacher.getNameDisplay()).append(" I know ").append(leader.getName()).append(" become Leader of ").append(getDisplayName()).append(".");
+                System.out.print(sb.toString() + "\n");
+            }
+        }else {
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+    public void appendMember(Student member){
+        members.add(member);
+        StringBuilder sb=new StringBuilder();
+        if(teacher!=null) {
+            sb.append(teacher.getNameDisplay()).append(" I know ").append(member.getName()).append(" has joined ").append(getDisplayName()).append(".");
+            System.out.print(sb.toString() + "\n");
+        }
+    }
+
+    public boolean isIn(Student student){
+
+        for (Student member:members){
+            if(member.getId()==student.getId()){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
 }
--- src/main/java/practice11/Student.java
+++ src/main/java/practice11/Student.java
@@ -1,4 +1,27 @@
 package practice11;
 
-public class Student {
+public class Student extends Person{
+    Klass klass;
+
+    public Student(int id,String name, int age, Klass klass) {
+        super(id,name, age);
+        this.klass = klass;
+        klass.members.add(this);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduce(){
+        StringBuilder sb=new StringBuilder();
+        sb.append(super.introduce()).append(" I am a Student. ");
+        if(klass.getLeader()!=null&&id==klass.getLeader().id){
+            sb.append("I am Leader of ");
+        }else {
+            sb.append("I am at ");
+        }
+        sb.append(klass.getDisplayName()).append(".");
+        return sb.toString();
+    }
 }
