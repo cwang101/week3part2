49a6797392e9edf242454f7b9dd56a3a10ec42a7
cwang101 <625250454@qq.com>
2018-04-13 13:54:33 +0800

    The practice02 complete!

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,19 @@
 package practice02;
 
-public class Student {
+public class Student extends Person {
+    int klass;
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+    public String introduce(){
+        StringBuilder sb=new StringBuilder();
+        sb.append("I am a Student. I am at Class ").append(klass).append(".");
+        return sb.toString();
+    }
 }
