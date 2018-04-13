44f2bf46d9d8bffac46238f6e9d833f2ba8465ec
cwang101 <625250454@qq.com>
2018-04-13 15:35:27 +0800

    The practice09 complete!

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,37 @@
 package practice09;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+    int number;
+    Student leader;
+    List<Student> members=new ArrayList<>();
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName(){
+        StringBuilder sb=new StringBuilder();
+        sb.append("Class ").append(number);
+        return sb.toString();
+    }
+    public void assignLeader(Student leader){
+        if(members.contains(leader)){
+            this.leader=leader;
+        }else {
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+    public void appendMember(Student member){
+        members.add(member);
+    }
 }
--- src/main/java/practice09/Person.java
+++ src/main/java/practice09/Person.java
@@ -1,4 +1,45 @@
 package practice09;
 
+import java.util.Objects;
+
 public class Person {
+    String name;
+    int age;
+    int id;
+
+    public Person(int id,String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.id=id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce(){
+        StringBuilder sb=new StringBuilder();
+        sb.append("My name is ").append(name).append(".").append(" I am ").append(age).append(" years old.");
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age &&
+                id == person.id &&
+                Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(name, age, id);
+    }
 }
