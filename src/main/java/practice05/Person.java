db966e4c5c53a09385443436ab0e596fee48f91a
cwang101 <625250454@qq.com>
2018-04-13 14:17:08 +0800

    The practice05 complete!

--- src/main/java/practice05/Person.java
+++ src/main/java/practice05/Person.java
@@ -1,4 +1,25 @@
 package practice05;
 
 public class Person {
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce(){
+        StringBuilder sb=new StringBuilder();
+        sb.append("My name is ").append(name).append(".").append(" I am ").append(age).append(" years old.");
+        return sb.toString();
+    }
 }
--- src/main/java/practice05/Student.java
+++ src/main/java/practice05/Student.java
@@ -1,4 +1,19 @@
 package practice05;
 
-public class Student {
+public class Student extends Person {
+    int klass;
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+    public String introduce(){
+        StringBuilder sb=new StringBuilder();
+        sb.append(super.introduce()).append(" I am a Student. I am at Class ").append(klass).append(".");
+        return sb.toString();
+    }
 }
