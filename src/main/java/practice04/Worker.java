2b81077e08dd479d911d18a2444858b378e5b3ac
cwang101 <625250454@qq.com>
2018-04-13 14:02:46 +0800

    The practice04 complete!

--- src/main/java/practice04/Worker.java
+++ src/main/java/practice04/Worker.java
@@ -1,4 +1,12 @@
 package practice04;
 
-public class Worker {
+public class Worker extends Person{
+    public Worker(String name, int age) {
+        super(name, age);
+    }
+    public String introduce(){
+        StringBuilder sb=new StringBuilder();
+        sb.append(super.introduce()).append(" I am a Worker. I have a job.");
+        return sb.toString();
+    }
 }
