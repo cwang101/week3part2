779d386ecbe00880dffa6ec39006ea4ac47ba3ed
cwang101 <625250454@qq.com>
2018-04-13 15:17:36 +0800

    The practice08 complete!

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,26 @@
 package practice08;
 
 public class Klass {
+    int number;
+    Student leader;
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName(){
+        StringBuilder sb=new StringBuilder();
+        sb.append("Class ").append(number);
+        return sb.toString();
+    }
+    public void assignLeader(Student leader){
+       this.leader=leader;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
 }
--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -1,4 +1,39 @@
 package practice08;
 
-public class Teacher {
+public class Teacher extends Person{
+    Klass klass;
+
+    public Teacher(int id,String name, int age, Klass klass) {
+        super(id,name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(int id,String name, int age) {
+        super(id,name, age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduce(){
+        StringBuilder sb=new StringBuilder();
+        if(klass!=null){
+            sb.append(super.introduce()).append(" I am a Teacher. I teach ").append(klass.getDisplayName()).append(".");
+        }else{
+            sb.append(super.introduce()).append(" I am a Teacher. I teach No Class.");
+        }
+
+        return sb.toString();
+    }
+    public String introduceWith(Student student){
+        StringBuilder sb=new StringBuilder();
+        if(klass!=null&&klass.getNumber()==student.getKlass().getNumber()){
+            sb.append(super.introduce()).append(" I am a Teacher. I teach ").append(student.getName()).append(".");
+        }else {
+            sb.append(super.introduce()).append(" I am a Teacher. I don't teach ").append(student.getName()).append(".");
+
+        }
+        return sb.toString();
+    }
 }
