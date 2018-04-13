e37aab3812f7b320262b0f43e20b8e6e03a9639c
cwang101 <625250454@qq.com>
2018-04-13 14:23:27 +0800

    The practice06 complete!

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,26 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    int klass;
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+    public int getKlass() {
+        return klass;
+    }
+    public String introduce(){
+        StringBuilder sb=new StringBuilder();
+        if(klass>0){
+            sb.append(super.introduce()).append(" I am a Teacher. I teach Class ").append(klass).append(".");
+        }else{
+            sb.append(super.introduce()).append(" I am a Teacher. I teach No Class.");
+        }
+
+        return sb.toString();
+    }
 }
